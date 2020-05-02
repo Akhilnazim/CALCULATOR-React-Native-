1ceb491f297077dc7a283975e1b59ee99bf2b8a7
Akhil Nazim <anunazim1@gmail.com>
2020-05-02 16:34:55 +0530

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,174 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      resultText: "",
+      calculatorText: "",
+    };
+    this.operations = ["DEL", "+", "-", "*", "/"];
+  }
+  calculateResult() {
+    const text = this.state.resultText;
+    this.setState({
+      calculatorText: eval(text),
+    });
+  }
+  validate() {
+    const text = this.state.resultText;
+    switch (text.slice(-1)) {
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        return false;
+    }
+    return true;
+  }
+  buttonPressed(text) {
+    // console.log(text);
+    if (text == "=") {
+      return this.validate() && this.calculateResult();
+    }
+    this.setState({ resultText: this.state.resultText + text });
+  }
+  operate(operations) {
+    switch (operations) {
+      case "DEL":
+        let text = this.state.resultText.split("");
+        text.pop();
+        text.join("");
+        this.setState({ resultText: text.join("") });
+        break;
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        const lastChar = this.state.resultText.split("").pop();
+        if (this.operations.indexOf(lastChar) > 0) return;
+        if (this.state.text == "") return;
+        this.setState({ resultText: this.state.resultText + operations });
+    }
+  }
+  render() {
+    let rows = [];
+    let nums = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [0, ".", "="],
+    ];
+    for (let i = 0; i < 4; i++) {
+      let row = [];
+      for (let j = 0; j < 3; j++) {
+        row.push(
+          <TouchableOpacity
+            key={nums[i][j]}
+            onPress={() => this.buttonPressed(nums[i][j])}
+            style={styles.btn}
+          >
+            <Text style={styles.btnText}>{nums[i][j]}</Text>
+          </TouchableOpacity>
+        );
+      }
+      rows.push(
+        <View key={i} style={styles.row}>
+          {row}
+        </View>
+      );
+    }
+
+    let ops = [];
+    for (let i = 0; i < 5; i++) {
+      ops.push(
+        <TouchableOpacity
+          key={this.operations[i]}
+          style={styles.btn}
+          onPress={() => this.operate(this.operations[i])}
+        >
+          <Text style={[styles.btnText, styles.white]}>
+            {this.operations[i]}
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <View style={styles.result}>
+          <Text style={styles.resultText}>{this.state.resultText}</Text>
+        </View>
+        <View style={styles.calculator}>
+          <Text style={styles.calculatorText}>{this.state.calculatorText}</Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}>{rows}</View>
+
+          <View style={styles.operations}>{ops}</View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  btnText: {
+    fontSize: 30,
+    color: "white",
+  },
+  white: {
+    color: "white",
+  },
+  btn: {
+    flex: 1,
+    alignItems: "center",
+    alignSelf: "stretch",
+    justifyContent: "center",
+  },
+  calculatorText: {
+    fontSize: 45,
+    color: "black",
+  },
+  resultText: {
+    fontSize: 60,
+    color: "black",
+  },
+
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  result: {
+    flex: 2,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  calculator: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  buttons: {
+    flex: 7,
+    flexDirection: "row",
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: "#434343",
+  },
+  operations: {
+    flex: 1,
+    justifyContent: "space-around",
+    backgroundColor: "#636363",
+    alignItems: "stretch",
   },
 });
